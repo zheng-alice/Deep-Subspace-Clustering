d8d9468075324b0e38a4fbee9f45e82e8aa44efa
Alexey Didenkov <aleksey.didenkov@gmail.com>
2018-10-31 14:28:54 -0400

    Print statements

--- full_model.py
+++ full_model.py
@@ -3,6 +3,7 @@ import img2matrix
 import matlab.engine
 import numpy as np
 import supporting_files.sda as sda
+import time
 from scipy.io import savemat, loadmat
 from sklearn.decomposition import PCA
 from sklearn.metrics import adjusted_rand_score as ari
@@ -15,33 +16,57 @@ from supporting_files.helpers import optimize
 #savemat('./saved/yaleB.mat', mdict={'rawX':images_dsift, 'X':images_norm, 'Y':labels})
 #images_dsift = loadmat("./saved/yaleB.mat")['rawX']
 #images_norm = loadmat("./saved/yaleB.mat")['X']
-#labels = loadmat("./saved/yaleB.mat")['Y']
+#labels = loadmat("./saved/yaleB.mat")['Y'].reshape(-1)
 
 def start_matlab():
+    print("\nStarting MATLAB engine...")
+    print("-------------------------")
+    start_time = time.time()
+    
     eng = matlab.engine.start_matlab()
     eng.cd("./SSC_ADMM_v1.1")
+
+    print("Elapsed: {0:.2f} sec".format(time.time()-start_time))
+    
     return eng
 
 def load_YaleB(path='./data/CroppedYale'):
-    train, test, img_size = img2matrix.batch_convert_YaleB(path, truncate_num=38, images_per_person=None)
+    print("\nLoading YaleB...")
+    print("----------------")
+    start_time = time.time()
 
+    train, test, img_size = img2matrix.batch_convert_YaleB(path, truncate_num=38, images_per_person=None)
     images_full = train[0]
     labels = train[1]
-
     images_raw = images_full[:, :32256]
     images_dsift = images_full[:, 32256:]
+
+    print("Elapsed: {0:.2f} sec".format(time.time()-start_time))
 
     return images_dsift, labels
 
 def preprocess(images_dsift):
+    print("\nPerforming PCA...")
+    print("-----------------")
+    start_time = time.time()
+    
     # Perform PCA
     pca = PCA(n_components=300, whiten=False, svd_solver='arpack', random_state=0)
     images_pca = pca.fit_transform(images_dsift)
+
+    print("Elapsed: {0:.2f} sec".format(time.time()-start_time))
+
+
+    print("\nNormalizing data...")
+    print("-------------------")
+    start_time = time.time()
 
     # Normalize PCA output
     mmin = np.min(images_pca)
     mmax = np.max(images_pca)
     images_norm = (2*images_pca - mmax - mmin) / (mmax - mmin)
+
+    print("Elapsed: {0:.2f} sec".format(time.time()-start_time))
 
     return images_norm
 
@@ -49,20 +74,32 @@ def run_model(images_norm,
               labels,
               epochs_pretrain = 101,
               epochs = 101,
-              alpha1 = 20,
-              maxIter1 = 6,
               lr_pretrain = 0.08,
               lr = 0.006,
+              #loss_limit = 0.001,
+              alpha1 = 20,
+              maxIter1 = 6,
               lambda1 = 0.0001,
               lambda2 = 0.001,
               alpha2 = 20,
               maxIter2 = 16):
+    print("\nFinding affinity matrix...")
+    print("--------------------------")
+    start_time = time.time()
+
     # Calculate C matrix
     # Matlab SSC #1
     savemat('./temp.mat', mdict={'X': images_norm})
     k = len(np.unique(labels))
     eng.SSC_modified(k, 0, False, float(alpha1), False, 1, 1e-20, maxIter1)
     C = loadmat("./temp.mat")['C']
+
+    print("Elapsed: {0:.2f} sec".format(time.time()-start_time))
+
+
+    print("\nTraining Autoencoder...")
+    print("-----------------------")
+    start_time = time.time()
 
     # Train Autoencoder
     d = dsc.DeepSubspaceClustering(images_norm, C=C, hidden_dims=[200, 150, 200], lambda1=lambda1, lambda2=lambda2, learning_rate=lr,
@@ -71,11 +108,20 @@ def run_model(images_norm,
     d.train(batch_size=images_norm.shape[0], epochs=epochs, print_step=25)
     images_HM2 = d.result
 
+    print("Elapsed: {0:.2f} sec".format(time.time()-start_time))
+
+
+    print("\nClustering with SSC...")
+    print("----------------------")
+    start_time = time.time()
+    
     # Cluster
     # Matlab SSC #2
     savemat('./temp.mat', mdict={'X': images_HM2})
     grps = eng.SSC_modified(k, 0, False, float(alpha2), False, 1, 1e-20, maxIter2)
     labels_pred = np.asarray(grps, dtype=np.int32).flatten()
+
+    print("Elapsed: {0:.2f} sec".format(time.time()-start_time))
 
     # Evaluate
     return 1-err_rate(labels, labels_pred), nmi(labels, labels_pred), ari(labels, labels_pred)
