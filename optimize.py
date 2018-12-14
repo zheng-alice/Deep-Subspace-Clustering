821e9c282624c4ee5616e7e74335cd9becbd4721
Alexey Didenkov <aleksey.didenkov@gmail.com>
2018-12-14 15:34:20 -0500

    Docstrings + name fixes

--- optimize.py
+++ optimize.py
@@ -76,7 +76,7 @@ def objective(hyper_params):
         global seed_, verb_model_
         try:
             fixed_params_copy.update(hyper_params)
-            return 1-fixed_params_copy.pop('model')(seed = seed_, verbose=verb_model_, **fixed_params_copy)[0]
+            return 1-fixed_params_copy.pop('model')(seed=seed_, verbose=verb_model_, **fixed_params_copy)[0]
         except Exception as ex:
             if(type(ex) == KeyError):
                 raise ex
@@ -84,7 +84,56 @@ def objective(hyper_params):
             return 1
     return internal(hyper_params)
 
-def optimize(function, fixed_params, iterations, random_seed, verb_model=False, verb=True):
+def optimize(function, fixed_params, iterations, random_seed=None, verb_model=False, verb=True):
+    """ Find optimum hyperparameters by repeatedly training the model from scratch.
+
+        PARAMETERS
+        ----------
+        function [callable]:
+            The function to use for optimization.
+            One of:
+                skopt.gp_minimize
+                skopt.dummy_minimize
+                skopt.forest_minimize
+                skopt.gbrt_minimize
+
+        fixed_params [dict of str:callable]:
+            Parameters that define the optimization.
+            Examples can be retrieved from get_params().
+
+            model [callable]:
+                The model to train.
+
+            n_rand [int]:
+                The number of iterations to randomly sample
+                points for before fitting a model.
+            
+            space [list]:
+                Hyperparameters to find and their limits.
+                Same requirements as that of the optimization method
+                All of these get passed to the model
+                under their respective names.
+
+            Any additional entries will be passed to the model.
+
+        iterations [int]:
+            Number of iterations to run the model for.
+
+        random_seed [int or None, default=None]:
+            Passed to the optimizer and the model
+        
+        verb_model [bool, default=False]:
+            Whether to pass verbose=True to the model
+        
+        verb [bool, default=True]:
+            Whether to print info at each iteration
+
+        RETURNS
+        -------
+        result [OptimizeResult]:
+            Result of the optimization, as returned by the optimization method
+            Can be saved through dump(), and resumed later through reload()
+    """
     start_time = time.time()
     
     # global b/c I couldn't find a better way to directly pass these to the objective function
@@ -115,7 +164,56 @@ def func_new(hyper_params):
             return y
     return func_(hyper_params)
 
-def reload(result, fixed_params, addtl_iters, random_seed, verb_model=False, verb=True):
+def reload(result, fixed_params, addtl_iters, random_seed=None, verb_model=False, verb=True):
+    """ Resume a previous optimization.
+
+        PARAMETERS
+        ----------
+        result [OptimizeResult]:
+            The previously returned optimization.
+            Tested functions:
+                skopt.gp_minimize
+                skopt.dummy_minimize
+                skopt.forest_minimize
+                skopt.gbrt_minimize
+
+        fixed_params [dict of str:callable]:
+            Parameters that define the optimization.
+            Examples can be retrieved from get_params().
+
+            model [callable]:
+                The model to train.
+
+            n_rand [int]:
+                The number of iterations to randomly sample
+                points for before fitting a model.
+            
+            space [list]:
+                Hyperparameters to find and their limits.
+                Same requirements as that of the optimization method
+                All of these get passed to the model
+                under their respective names.
+
+            Any additional entries will be passed to the model.
+
+        addtl_iters [int]:
+            Number of additional iterations to run the model for.
+
+        random_seed [int or None, default=None]:
+            Passed to the optimizer and the model
+        
+        verb_model [bool, default=False]:
+            Whether to pass verbose=True to the model
+        
+        verb [bool, default=True]:
+            Whether to print info at each iteration
+
+        RETURNS
+        -------
+        result_new [OptimizeResult]:
+            The updated result of the optimization, as returned by the optimization method
+            Can be saved through dump(), and resumed once again
+    """
     start_time = time.time()
     
     # since objective relies on global variables, set them again
@@ -157,22 +255,67 @@ def reload(result, fixed_params, addtl_iters, random_seed, verb_model=False, ver
 
 
 def optimize_multiple(scenario, iterations, seeds=range(5), functions={"gp":gp_minimize, "dummy":dummy_minimize, "forest":forest_minimize, "gbrt":gbrt_minimize}, verb_model=False, verb=False):
+    """ Call optimize() across several functions and seeds
+
+        PARAMETERS
+        ----------
+        scenario [int]:
+            id of the scenario.
+            Used to get_params().
+            Determines the directory to save in.
+
+        iterations [int]:
+            Number of iterations to run the model for.
+            Used in filenames upon saving.
+
+        seeds [list of int]:
+            Values to be passed as seeds.
+            Used in filenames upon saving.
+
+        functions [dict of str:callable]:
+            Optimization functions to call and their respective names.
+            Names are used in the filenames upon saving.
+    """
     fixed_params = get_params(scenario)
     if(not os.path.isdir("optims/scenario" + str(scenario))):
         os.mkdir("optims/scenario" + str(scenario))
     
     for seed in seeds:
         print("Seed: " + str(seed))
-        for func_name in function_dict.keys():
+        for func_name in functions.keys():
             print(func_name + ':')
-            result = optimize(function_dict[func_name], fixed_params, iterations, seed, verb_model=verb_model, verb=verb)
+            result = optimize(functions[func_name], fixed_params, iterations, seed, verb_model=verb_model, verb=verb)
             dump(result, "optims/scenario" + str(scenario) + '/' + func_name + '_' + str(seed) + "_" + str(iterations) + ".opt")
 
 def reload_multiple(scenario, init_iters, addtl_iters, seeds=range(5), func_names=["gp", "dummy", "forest", "gbrt"], verb_model=False, verb=False):
+    """ Call reload() across several functions and seeds
+
+        PARAMETERS
+        ----------
+        scenario [int]:
+            id of the scenario.
+            Used to get_params().
+            Determines the directory to save in.
+
+        init_iters [int]:
+            Iteration count of optimizations to load.
+
+        addtl_iters [int]:
+            Number of additional iterations to run the model for.
+            New total is used in filenames upon saving.
+
+        seeds [list of int]:
+            Values to be passed as seeds.
+            Used in filenames upon saving.
+
+        func_names [list of str]:
+            Names of optimization functions to reload.
+            Used in filenames upon saving.
+    """
     fixed_params = get_params(scenario)
     for seed in seeds:
         print("Seed: " + str(seed))
-        for func_name in function_names:
+        for func_name in func_names:
             print(func_name + ':')
             result_loaded = load("optims/scenario" + str(scenario) + '/' + func_name + '_' + str(seed) + "_" + str(init_iters) + ".opt")
             result = reload(result_loaded, fixed_params, addtl_iters, seed, verb_model=verb_model, verb=verb)
