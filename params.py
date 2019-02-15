5ecf4ada730977edaf07e42977dedb7a054a5d0f
Alexey Didenkov <aleksey.didenkov@gmail.com>
2019-02-15 14:10:59 -0500

    Reduced maxIter

--- params.py
+++ params.py
@@ -55,34 +55,60 @@ all_params = [
         Real(10**-5, 10**-1, "log-uniform", name='lr'),
         Real(10**-1, 10**3, "log-uniform", name='alpha2'),
         Integer(10, 200, name='maxIter2')]},
-  {'model':run_ssc, 'dataset':'Coil20', 'n_rand':160, 'maxIter':250, 'space':
+  {'model':run_ssc, 'dataset':'Coil20', 'n_rand':160, 'maxIter':63, 'space':
        [Real(10**0, 10**4, "log-uniform", name='alpha')]},
   # 10
   {'model':run_ae, 'dataset':'Coil20', 'n_rand':160, 'epochs_pretrain':1000, 'epochs':250,
+   'maxIter2':63, 'space':
+       [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
+        Real(10**-6, 10**-2, "log-uniform", name='lr'),
+        Real(10**-3, 10**1, "log-uniform", name='lambda2'),
+        Real(10**0, 10**4, "log-uniform", name='alpha2')]},
+  {'model':run_model, 'dataset':'Coil20', 'n_rand':160, 'maxIter1':63, 'epochs_pretrain':1000,
+   'epochs':250, 'maxIter2':63, 'trainC':False, 'giveC':False, 'space':
+       [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
+        Real(10**0, 10**4, "log-uniform", name='alpha1'),
+        Real(10**-6, 10**-2, "log-uniform", name='lr'),
+        Real(10**-2, 10**2, "log-uniform", name='lambda1'),
+        Real(10**-3, 10**1, "log-uniform", name='lambda2'),
+        Real(10**0, 10**4, "log-uniform", name='alpha2')]},
+  {'model':run_model, 'dataset':'Coil20', 'n_rand':160, 'maxIter1':63, 'epochs_pretrain':1000,
+   'epochs':250, 'maxIter2':63, 'trainC':True, 'giveC':False, 'space':
+       [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
+        Real(10**0, 10**4, "log-uniform", name='alpha1'),
+        Real(10**-6, 10**-2, "log-uniform", name='lr'),
+        Real(10**-2, 10**2, "log-uniform", name='lambda1'),
+        Real(10**-3, 10**1, "log-uniform", name='lambda2'),
+        Real(10**1, 10**5, "log-uniform", name='lambda3'),
+        Real(10**0, 10**4, "log-uniform", name='alpha2')]},
+  {'model':run_model, 'dataset':'Coil20', 'n_rand':160, 'maxIter1':63, 'epochs_pretrain':1000,
+   'epochs':250, 'maxIter2':63, 'trainC':True, 'giveC':True, 'space':
+       [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
+        Real(10**0, 10**4, "log-uniform", name='alpha1'),
+        Real(10**-6, 10**-2, "log-uniform", name='lr'),
+        Real(10**-2, 10**2, "log-uniform", name='lambda1'),
+        Real(10**-3, 10**1, "log-uniform", name='lambda2'),
+        Real(10**1, 10**5, "log-uniform", name='lambda3'),
+        Real(10**0, 10**4, "log-uniform", name='alpha2')]},
+  {'model':run_ssc, 'dataset':'Coil20', 'n_rand':40, 'maxIter':250, 'space':
+       [Real(0.021193, 52.9834, "log-uniform", name='alpha')]},
+  # 15
+  {'model':run_ae, 'dataset':'Coil20', 'n_rand':40, 'epochs_pretrain':4000, 'epochs':1000,
    'maxIter2':250, 'space':
        [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
         Real(10**-6, 10**-2, "log-uniform", name='lr'),
         Real(10**-3, 10**1, "log-uniform", name='lambda2'),
         Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_model, 'dataset':'Coil20', 'n_rand':160, 'maxIter1':250, 'epochs_pretrain':1000,
-   'epochs':250, 'maxIter2':250, 'trainC':False, 'giveC':False, 'space':
+  {'model':run_model, 'dataset':'Coil20', 'n_rand':40, 'maxIter1':250, 'epochs_pretrain':4000,
+   'epochs':1000, 'maxIter2':250, 'trainC':False, 'giveC':False, 'space':
        [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
         Real(10**0, 10**4, "log-uniform", name='alpha1'),
         Real(10**-6, 10**-2, "log-uniform", name='lr'),
         Real(10**-2, 10**2, "log-uniform", name='lambda1'),
         Real(10**-3, 10**1, "log-uniform", name='lambda2'),
         Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_model, 'dataset':'Coil20', 'n_rand':160, 'maxIter1':250, 'epochs_pretrain':1000,
-   'epochs':250, 'maxIter2':250, 'trainC':True, 'giveC':False, 'space':
-       [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
-        Real(10**0, 10**4, "log-uniform", name='alpha1'),
-        Real(10**-6, 10**-2, "log-uniform", name='lr'),
-        Real(10**-2, 10**2, "log-uniform", name='lambda1'),
-        Real(10**-3, 10**1, "log-uniform", name='lambda2'),
-        Real(10**1, 10**5, "log-uniform", name='lambda3'),
-        Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_model, 'dataset':'Coil20', 'n_rand':160, 'maxIter1':250, 'epochs_pretrain':1000,
-   'epochs':250, 'maxIter2':250, 'trainC':True, 'giveC':True, 'space':
+  {'model':run_model, 'dataset':'Coil20', 'n_rand':40, 'maxIter1':250, 'epochs_pretrain':4000,
+   'epochs':1000, 'maxIter2':250, 'trainC':True, 'giveC':False, 'space':
        [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
         Real(10**0, 10**4, "log-uniform", name='alpha1'),
         Real(10**-6, 10**-2, "log-uniform", name='lr'),
@@ -90,34 +116,34 @@ all_params = [
         Real(10**-3, 10**1, "log-uniform", name='lambda2'),
         Real(10**1, 10**5, "log-uniform", name='lambda3'),
         Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_ssc, 'dataset':'Coil20', 'n_rand':40, 'maxIter':1000, 'space':
-       [Real(0.021193, 52.9834, "log-uniform", name='alpha')]},
-  # 15
-  {'model':run_ae, 'dataset':'Coil20', 'n_rand':40, 'epochs_pretrain':4000, 'epochs':1000,
+  {'model':run_model, 'dataset':'Coil20', 'n_rand':40, 'maxIter1':250, 'epochs_pretrain':4000,
+   'epochs':1000, 'maxIter2':250, 'trainC':True, 'giveC':True, 'space':
+       [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
+        Real(10**0, 10**4, "log-uniform", name='alpha1'),
+        Real(10**-6, 10**-2, "log-uniform", name='lr'),
+        Real(10**-2, 10**2, "log-uniform", name='lambda1'),
+        Real(10**-3, 10**1, "log-uniform", name='lambda2'),
+        Real(10**1, 10**5, "log-uniform", name='lambda3'),
+        Real(10**0, 10**4, "log-uniform", name='alpha2')]},
+  {'model':run_ssc, 'dataset':'Coil20', 'n_rand':10, 'maxIter':1000, 'space':
+       [Real(0.036796, 22.99753, "log-uniform", name='alpha')]},
+  # 20
+  {'model':run_ae, 'dataset':'Coil20', 'n_rand':10, 'epochs_pretrain':16000, 'epochs':1000,
    'maxIter2':1000, 'space':
        [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
         Real(10**-6, 10**-2, "log-uniform", name='lr'),
         Real(10**-3, 10**1, "log-uniform", name='lambda2'),
         Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_model, 'dataset':'Coil20', 'n_rand':40, 'maxIter1':1000, 'epochs_pretrain':4000,
-   'epochs':1000, 'maxIter2':1000, 'trainC':False, 'giveC':False, 'space':
+  {'model':run_model, 'dataset':'Coil20', 'n_rand':10, 'maxIter1':1000, 'epochs_pretrain':16000,
+   'epochs':4000, 'maxIter2':1000, 'trainC':False, 'giveC':False, 'space':
        [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
         Real(10**0, 10**4, "log-uniform", name='alpha1'),
         Real(10**-6, 10**-2, "log-uniform", name='lr'),
         Real(10**-2, 10**2, "log-uniform", name='lambda1'),
         Real(10**-3, 10**1, "log-uniform", name='lambda2'),
         Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_model, 'dataset':'Coil20', 'n_rand':40, 'maxIter1':1000, 'epochs_pretrain':4000,
-   'epochs':1000, 'maxIter2':1000, 'trainC':True, 'giveC':False, 'space':
-       [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
-        Real(10**0, 10**4, "log-uniform", name='alpha1'),
-        Real(10**-6, 10**-2, "log-uniform", name='lr'),
-        Real(10**-2, 10**2, "log-uniform", name='lambda1'),
-        Real(10**-3, 10**1, "log-uniform", name='lambda2'),
-        Real(10**1, 10**5, "log-uniform", name='lambda3'),
-        Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_model, 'dataset':'Coil20', 'n_rand':40, 'maxIter1':1000, 'epochs_pretrain':4000,
-   'epochs':1000, 'maxIter2':1000, 'trainC':True, 'giveC':True, 'space':
+  {'model':run_model, 'dataset':'Coil20', 'n_rand':10, 'maxIter1':1000, 'epochs_pretrain':16000,
+   'epochs':4000, 'maxIter2':1000, 'trainC':True, 'giveC':False, 'space':
        [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
         Real(10**0, 10**4, "log-uniform", name='alpha1'),
         Real(10**-6, 10**-2, "log-uniform", name='lr'),
@@ -125,34 +151,8 @@ all_params = [
         Real(10**-3, 10**1, "log-uniform", name='lambda2'),
         Real(10**1, 10**5, "log-uniform", name='lambda3'),
         Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_ssc, 'dataset':'Coil20', 'n_rand':10, 'maxIter':4000, 'space':
-       [Real(0.036796, 22.99753, "log-uniform", name='alpha')]},
-  # 20
-  {'model':run_ae, 'dataset':'Coil20', 'n_rand':10, 'epochs_pretrain':16000, 'epochs':4000,
-   'maxIter2':4000, 'space':
-       [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
-        Real(10**-6, 10**-2, "log-uniform", name='lr'),
-        Real(10**-3, 10**1, "log-uniform", name='lambda2'),
-        Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_model, 'dataset':'Coil20', 'n_rand':10, 'maxIter1':4000, 'epochs_pretrain':16000,
-   'epochs':4000, 'maxIter2':4000, 'trainC':False, 'giveC':False, 'space':
-       [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
-        Real(10**0, 10**4, "log-uniform", name='alpha1'),
-        Real(10**-6, 10**-2, "log-uniform", name='lr'),
-        Real(10**-2, 10**2, "log-uniform", name='lambda1'),
-        Real(10**-3, 10**1, "log-uniform", name='lambda2'),
-        Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_model, 'dataset':'Coil20', 'n_rand':10, 'maxIter1':4000, 'epochs_pretrain':16000,
-   'epochs':4000, 'maxIter2':4000, 'trainC':True, 'giveC':False, 'space':
-       [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
-        Real(10**0, 10**4, "log-uniform", name='alpha1'),
-        Real(10**-6, 10**-2, "log-uniform", name='lr'),
-        Real(10**-2, 10**2, "log-uniform", name='lambda1'),
-        Real(10**-3, 10**1, "log-uniform", name='lambda2'),
-        Real(10**1, 10**5, "log-uniform", name='lambda3'),
-        Real(10**0, 10**4, "log-uniform", name='alpha2')]},
-  {'model':run_model, 'dataset':'Coil20', 'n_rand':10, 'maxIter1':4000, 'epochs_pretrain':16000,
-   'epochs':4000, 'maxIter2':4000, 'trainC':True, 'giveC':True, 'space':
+  {'model':run_model, 'dataset':'Coil20', 'n_rand':10, 'maxIter1':1000, 'epochs_pretrain':16000,
+   'epochs':4000, 'maxIter2':1000, 'trainC':True, 'giveC':True, 'space':
        [Real(10**-6, 10**-2, "log-uniform", name='lr_pretrain'),
         Real(10**0, 10**4, "log-uniform", name='alpha1'),
         Real(10**-6, 10**-2, "log-uniform", name='lr'),
