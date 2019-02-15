460f7b7da09457448c8171a4ab2d316f6b4e69e5
Alexey Didenkov <aleksey.didenkov@gmail.com>
2019-02-14 21:53:23 -0500

    Matlab hyperopt visualizer

--- figures/hyper_opt.m
+++ figures/hyper_opt.m
@@ -1,15 +1,70 @@
 load hyper_opt.mat;
+axxes = containers.Map(fieldnames(axxes), struct2cell(axxes));
 
-colormap(jet);
+if axxes.Count < 2
+    error("Data has too few dimensions: " + axxes.Count);
+end
+idx_x = 1;
+idx_y = 2;
 
-c = linspace(0, 1, size(y, 2));
-truth = scatter3(log(x(:, 1)), x(:, 2), log(y), size(y, 2), c, 'filled');
+
+M = size(truth.y, 1);
+best = truth.x(truth.best, :);
+for i = 1:size(best, 2)
+    % convert to surrogate indices
+    [~, idx] = min(abs(axxes(deblank(truth.names(i, :)))-best(i)));
+    best(i) = idx;
+end
+slice = num2cell(best);
+slice{idx_x} = ':';
+slice{idx_y} = ':';
+
+
+% create colors and size
+% size decreases with distance to visible plane
+c = linspace(0, 1, M);
+s = 50.* ones(M, 1);
+hidden = 1:axxes.Count;
+hidden([idx_x, idx_y]) = [];
+plane = 1:size(hidden, 2);
+range = 1:size(hidden, 2);
+values = truth.x(:, hidden);
+for i = plane
+    axis = axxes(deblank(truth.names(i, :)));
+    plane(i) = axis(slice{hidden(i)});
+    range(i) = axis(size(axis, 1));
+    if strcmp(deblank(truth.priors(i, :)), 'log')
+       plane(i) = log10(plane(i));
+       range(i) = log10(range(i)) - log10(axis(1));
+       values(:, i) = log10(values(:, 1));
+    else
+       range(i) = range(i) - axis(1);
+    end
+end
+dist = sqrt(sum(((values - plane)./ range).^2, 2));
+s = s.* (1.- (dist./sqrt(max(1, size(hidden, 2)))));
+plot_truth = scatter3(truth.x(:, idx_x), truth.x(:, idx_y), truth.y, s, c, 'filled');
 
 hold on;
 
-maxIter = 10.^linspace(1, 26/20+1, 27);
-alpha = 10.^linspace(-1, 40/20-1, 41);
-surrogate = surf(log(alpha), maxIter, log(surface));
+means = surrogate.mean(slice{:});
+stds = surrogate.std(slice{:});
+if idx_x < idx_y
+    means = means.';
+    stds = stds.';
+end
+plot_surrogate = surf(axxes(deblank(truth.names(idx_x, :))), axxes(deblank(truth.names(idx_y, :))), means);
+set(plot_surrogate, 'EdgeAlpha', 'interp', 'FaceColor', 'interp', 'FaceAlpha', 'interp');
+set(plot_surrogate, 'AlphaData', 10.^-stds);
+
+
+if strcmp(deblank(truth.priors(idx_x, :)), 'log')
+    set(gca, 'Xscale', 'log');
+end
+if strcmp(deblank(truth.priors(idx_y, :)), 'log')
+    set(gca, 'Yscale', 'log');
+end
+set(gca, 'Zscale', 'log');
 
 % Create figure window and components
 
