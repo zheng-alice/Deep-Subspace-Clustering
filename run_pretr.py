9c02281d721c6acdbedb5874dfd3ebb1af460932
Alexey Didenkov <aleksey.didenkov@gmail.com>
2019-05-17 11:07:37 -0400

    Model cleaning sorting fix

--- run_pretr.py
+++ run_pretr.py
@@ -5,7 +5,7 @@ import glob
 import os
 
 def clean(path):
-    for file in glob.glob(path)[5:]:
+    for file, loss in sorted([(file, float(file[path.index('*'):path.index('*')-len(path)+1])) for file in glob.glob(path)], key=lambda x:x[1])[5:]:
         file_remove = file.replace('\\', '/')
         os.remove(file_remove)
         print("Deleted " + file_remove)
@@ -22,7 +22,7 @@ opt_params = {'model':pretrain, 'dataset':'Coil20', 'n_rand':10, \
                                     'sda_printstep': -100, \
                                     'validation_step': 10, \
                                     'stop_criteria': 3}, \
-              'space': [Real(1.0E-05, 1.0E-01, "log-uniform", name='lr'), \
+              'space': [Real(1.0E-06, 1.0E-00, "log-uniform", name='lr'), \
                         Integer(1, 200, name='batch_num')],
               'save_path':"./saved/models/coil20/256.64_10000.10.3_{0:.4g}"}
 data_loaded = loadmat("./saved/rescaled/" + opt_params.pop('dataset'))
